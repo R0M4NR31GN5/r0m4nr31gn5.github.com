7f5ad924c7bbb8fa4e7ea5c77fed0a104c8b20ce
R0M4NR31GN5 <155585667+R0M4NR31GN5@users.noreply.github.com>
2024-02-21 23:01:28 -0600

    Create rockPaperScissors.js

---
+++ Code/JavaScript Projects/rockPaperScissors.js
@@ -0,0 +1,61 @@
+console.log('Rock, Paper, Scissors, Shoot!');
+
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    return userInput;
+  } else {
+    console.log('That\'s not an input. Try again :)')
+  }
+}
+
+const getComputerChoice = () => {
+  computerInput = Math.floor(Math.random() * 3) ;
+  if (computerInput === 0) {
+    return 'rock';
+  } else if (computerInput === 1) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return 'TIE';
+  }
+
+  if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'COMPUTER WON!';
+    } else if (computerChoice === 'scissors') {
+      return 'YOU WON!';
+    }
+  } 
+
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'COMPUTER WON!'
+    } else if (computerChoice === 'rock') {
+      return 'YOU WON!';
+    }
+  }
+
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'COMPUTER WON!';
+    } else if (computerChoice === 'paper') {
+      return 'YOU WON!'
+    }
+  }
+}
+
+function playGame() {
+  const userChoice = getUserChoice('paper');
+  const computerChoice = getComputerChoice();
+  console.log(userChoice);
+  console.log(computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
